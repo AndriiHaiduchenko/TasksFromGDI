7388c6b2de3b0d23d8ed9ed5588582ac4bd566ff
Andrii <demonictoast@gmail.com>
2015-05-10 17:00:16 +0300

    First steps

---
+++ Matrix/Matrix/Matrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix 
+{
+    class Matrix<T> // T - parameter
+    {
+        public int rows;
+        public int cols;
+        public T[,] matrix;  //this will be changed later
+        Random random = new Random();
+
+
+        public Matrix(int rows, int cols) // Matrix constructor
+        {
+            this.cols = cols;
+            this.rows = rows;
+            matrix = new T[rows,cols];
+            
+        }
+
+        public T this[int iRows, int iCols] //indexers
+        {
+            get { return matrix[iRows, iCols]; }
+            set { matrix[iRows, iCols] = value; }
+        }
+
+        public bool IsMatrixSquare() //Checking if matrix is square
+        {
+            return (rows == cols);
+        }
+
+        
+
+       
+
+
+        
+    }
+}
---
+++ Matrix/Matrix/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Matrix<int> mat = new Matrix<int>(random.Next(1,30),random.Next(1,30)); //random instance of Matrix class
+            
+
+            for (int i = 0; i <mat.rows; i++)
+            
+            {
+                for (int j = 0; j <mat.cols; j++)
+                {
+                    mat[i, j] = random.Next(0, 10);
+                }
+            }
+
+            for (int i = 0; i <mat.rows; i++)
+            {
+                for (int j = 0; j <mat.cols; j++)
+                {
+                    Console.Write("{0} ", mat[i, j]);
+                }
+                Console.Write("\n");
+            }
+            Console.WriteLine( mat.rows);
+            Console.WriteLine( mat.cols );
+            Console.WriteLine(mat.IsMatrixSquare());
+
+          
+            
+        }
+    }
+}
